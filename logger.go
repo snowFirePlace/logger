668dfd5bf8a0da10c12c2ccb941db71742db2791
Serg Yash <snow.irk@gmail.com>
2020-12-04 08:06:02 +0800

    none

--- logger.go
+++ logger.go
@@ -109,31 +109,46 @@ func (l *Log) Fatalf(s string, args ...interface{}) {
 	os.Exit(1)
 }
 
-// func Debug(args ...interface{}) {
-// 	if std.Level.Number <= 2 {
-// 		if len(args) >= 2 {
-// 			a := fmt.Sprintln(args...)
-// 			std.out("DEBUG", a[:len(a)-1])
-// 			return
-// 		}
-// 		std.out("DEBUG", fmt.Sprint(args...))
-// 	}
-// }
-// func Info(args ...interface{}) {
-// 	if std.Level.Number <= 3 {
-// 		if len(args) >= 2 {
-// 			a := fmt.Sprintln(args...)
-// 			std.out("INFO ", a[:len(a)-1])
-// 			return
-// 		}
-// 		std.out("INFO", fmt.Sprint(args...))
-// 	}
-// }
-// func Error(args ...interface{}) {
-// 	if len(args) >= 2 {
-// 		a := fmt.Sprintln(args...)
-// 		std.out("ERROR", a[:len(a)-1])
-// 		return
-// 	}
-// 	std.out("ERROR", fmt.Sprint(args...))
-// }
+func Debug(args ...interface{}) {
+	if std.Level.Number <= 2 {
+		if len(args) >= 2 {
+			a := fmt.Sprintln(args...)
+			std.out("DEBUG", a[:len(a)-1])
+			return
+		}
+		std.out("DEBUG", fmt.Sprint(args...))
+	}
+}
+func Info(args ...interface{}) {
+	if std.Level.Number <= 3 {
+		if len(args) >= 2 {
+			a := fmt.Sprintln(args...)
+			std.out("INFO ", a[:len(a)-1])
+			return
+		}
+		std.out("INFO", fmt.Sprint(args...))
+	}
+}
+func Error(args ...interface{}) {
+	if len(args) >= 2 {
+		a := fmt.Sprintln(args...)
+		std.out("ERROR", a[:len(a)-1])
+		return
+	}
+	std.out("ERROR", fmt.Sprint(args...))
+}
+
+func Fatal(args ...interface{}) {
+	if len(args) >= 2 {
+		a := fmt.Sprintln(args...)
+		std.out("FATAL", a[:len(a)-1])
+	} else {
+		std.out("FATAL", fmt.Sprint(args...))
+	}
+	os.Exit(1)
+}
+
+func Fatalf(s string, args ...interface{}) {
+	std.out("FATAL", fmt.Sprintf(s, args...))
+	os.Exit(1)
+}
